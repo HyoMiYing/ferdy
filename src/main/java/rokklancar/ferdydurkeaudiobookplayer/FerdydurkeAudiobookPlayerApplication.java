d0d2649763df88bd8e03b16f153483bf11f9b424
rok <rklancar@gmail.com>
2023-03-15 17:18:12 +0100

    Initialized project with Spring Initializr

---
+++ src/main/java/rokklancar/ferdydurkeaudiobookplayer/FerdydurkeAudiobookPlayerApplication.java
@@ -0,0 +1,13 @@
+package rokklancar.ferdydurkeaudiobookplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerdydurkeAudiobookPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerdydurkeAudiobookPlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rokklancar/ferdydurkeaudiobookplayer/FerdydurkeAudiobookPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package rokklancar.ferdydurkeaudiobookplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerdydurkeAudiobookPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
