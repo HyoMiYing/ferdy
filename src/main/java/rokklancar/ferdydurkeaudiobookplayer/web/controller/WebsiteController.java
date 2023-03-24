52c5904f7b28794ea09ec154da0d2bc192028070
rok <rklancar@gmail.com>
2023-03-25 00:57:35 +0100

    DTO validation errors are now caught in @ExceptionHandler and displayed in HTML. there are also a bunch of dirty system.out.println() statemnts there for debugging purposes still there.

--- src/main/java/rokklancar/ferdydurkeaudiobookplayer/web/controller/WebsiteController.java
+++ src/main/java/rokklancar/ferdydurkeaudiobookplayer/web/controller/WebsiteController.java
@@ -5,12 +5,13 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.validation.ValidationErrors;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import rokklancar.ferdydurkeaudiobookplayer.persistence.dao.UserRepository;
 import rokklancar.ferdydurkeaudiobookplayer.service.IUserService;
 import rokklancar.ferdydurkeaudiobookplayer.web.dto.UserDto;
@@ -18,6 +19,7 @@ import rokklancar.ferdydurkeaudiobookplayer.persistence.model.User;
 import rokklancar.ferdydurkeaudiobookplayer.web.error.UserAlreadyExistsException;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class WebsiteController {
@@ -66,12 +68,11 @@ public class WebsiteController {
         return "register.html";
     }
 
-    @PostMapping("/neprijavljeni/registracija/submit")
+    @PostMapping("/neprijavljeni/registracija")
     public String register(
             @ModelAttribute("user") @Valid UserDto userDto,
-            HttpServletRequest request,
-            Errors errors) {
-
+            HttpServletRequest request
+    ) {
         try {
             final User registered = userService.registerNewUserAccount(userDto);
             log.info("User " + registered + " has been saved.");
@@ -93,5 +94,28 @@ public class WebsiteController {
     public String aboutPageAuthenticated(Principal principal, Model model) {
         model.addAttribute("authenticatedUserName", principal.getName());
         return "about_authenticated.html";
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ModelAndView validationError(BindException exception) {
+        BindingResult result = exception.getBindingResult();
+        final List<FieldError> fieldErrors = result.getFieldErrors();
+        final List<ObjectError> globalErrors = result.getGlobalErrors();
+        final List<ObjectError> allErrors = result.getAllErrors();
+        System.out.println("Field errors: " + fieldErrors);
+        System.out.println("BindingResutl: " + result);
+        System.out.println("BindingResutl global errors: " + globalErrors);
+        System.out.println("BindingResutl all errors: " + allErrors);
+        System.out.println("BindingResutl target: " + result.getTarget());
+
+        UserDto user = new UserDto();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("fieldErrors", fieldErrors);
+        modelAndView.addObject("globalErrors", globalErrors);
+        modelAndView.addObject("user", user);
+
+        modelAndView.setViewName("register.html");
+
+        return modelAndView;
     }
 }
