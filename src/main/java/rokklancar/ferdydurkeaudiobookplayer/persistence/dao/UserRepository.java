cdc7cbbf690f2a31b164f04d8fcdd886ca3b35c2
rok <rklancar@gmail.com>
2023-03-24 15:58:05 +0100

    Created UserRepository which makes use of JpaRepository interface. Imported JPA dependency

--- src/main/java/rokklancar/ferdydurkeaudiobookplayer/persistence/dao/UserRepository.java
+++ src/main/java/rokklancar/ferdydurkeaudiobookplayer/persistence/dao/UserRepository.java
@@ -1,4 +1,12 @@
 package rokklancar.ferdydurkeaudiobookplayer.persistence.dao;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import rokklancar.ferdydurkeaudiobookplayer.persistence.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+    @Override
+    void delete(User user);
 }
