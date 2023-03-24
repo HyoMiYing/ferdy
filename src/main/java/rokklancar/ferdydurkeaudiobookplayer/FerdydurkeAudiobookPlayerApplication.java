a4d740b97ae1d378ab11b5f0f0a6867c7eaedcae
rok <rklancar@gmail.com>
2023-03-24 20:34:19 +0100

    Added code where a User object is saved to database at startup, no password hashing no validation. Just to test database connectivity (I am looking at postgresql with pgAdmin4)

--- src/main/java/rokklancar/ferdydurkeaudiobookplayer/FerdydurkeAudiobookPlayerApplication.java
+++ src/main/java/rokklancar/ferdydurkeaudiobookplayer/FerdydurkeAudiobookPlayerApplication.java
@@ -1,13 +1,24 @@
 package rokklancar.ferdydurkeaudiobookplayer;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import rokklancar.ferdydurkeaudiobookplayer.persistence.dao.UserRepository;
+import rokklancar.ferdydurkeaudiobookplayer.persistence.model.User;
 
 @SpringBootApplication
 public class FerdydurkeAudiobookPlayerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FerdydurkeAudiobookPlayerApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo (UserRepository repository) {
+		return (args -> {
+			repository.save(new User("test@test.test", "testpassword"));
+		});
 	}
 
 }
