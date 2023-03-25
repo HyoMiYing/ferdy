f68f34d3f95b75cf5d24a1b53bf5ae44cb9335d1
rok <rklancar@gmail.com>
2023-03-25 14:21:47 +0100

    Implemented MyUserDetailsService, so that users are authenticated against saved users in the database.

---
+++ src/main/java/rokklancar/ferdydurkeaudiobookplayer/security/MyUserDetailsService.java
@@ -0,0 +1,43 @@
+package rokklancar.ferdydurkeaudiobookplayer.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import rokklancar.ferdydurkeaudiobookplayer.persistence.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import rokklancar.ferdydurkeaudiobookplayer.persistence.dao.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found with username: " + email);
+        }
+        boolean enabled = true;
+        boolean accountNonExpired = true;
+        boolean credentialsNonExpired = true;
+        boolean accountNonLocked = true;
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(), user.getPassword(), enabled, accountNonExpired,
+                credentialsNonExpired, accountNonLocked, getAuthorities());
+    }
+
+    private static List<GrantedAuthority> getAuthorities () {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        return authorities;
+    }
+}
--- src/main/java/rokklancar/ferdydurkeaudiobookplayer/security/configuration/SecurityConfiguration.java
+++ src/main/java/rokklancar/ferdydurkeaudiobookplayer/security/configuration/SecurityConfiguration.java
@@ -1,7 +1,10 @@
-package rokklancar.ferdydurkeaudiobookplayer.configuration;
+package rokklancar.ferdydurkeaudiobookplayer.security.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,9 +12,36 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import rokklancar.ferdydurkeaudiobookplayer.security.MyUserDetailsService;
 
 @Configuration
 public class SecurityConfiguration {
+
+    private MyUserDetailsService userDetailsService;
+
+    public SecurityConfiguration(MyUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .authenticationProvider(authProvider())
+                .build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(encoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(11);
+    }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
