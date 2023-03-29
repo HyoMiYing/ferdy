8b6d5794bc44e0344386e87cc32c359d87769c45
rok <rklancar@gmail.com>
2023-03-29 17:56:50 +0200

    Written a model for bookmark class

--- src/main/java/rokklancar/ferdydurkeaudiobookplayer/persistence/model/Bookmark.java
+++ src/main/java/rokklancar/ferdydurkeaudiobookplayer/persistence/model/Bookmark.java
@@ -1,4 +1,63 @@
 package rokklancar.ferdydurkeaudiobookplayer.persistence.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "bookmarks")
 public class Bookmark {
+
+    @Id
+    @Column(unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String chapter;
+
+    private String timestamp;
+
+    // Can be empty
+    private String note;
+
+    @ManyToOne
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getChapter() {
+        return chapter;
+    }
+
+    public void setChapter(String chapter) {
+        this.chapter = chapter;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
