2562011c843e3ccdd3fd960bbb9817741bb0943f
rok <rklancar@gmail.com>
2023-03-29 18:02:45 +0200

    Added getters and setters to BookmarkDto

--- src/main/java/rokklancar/ferdydurkeaudiobookplayer/web/dto/BookmarkDto.java
+++ src/main/java/rokklancar/ferdydurkeaudiobookplayer/web/dto/BookmarkDto.java
@@ -15,4 +15,36 @@ public class BookmarkDto {
 
     @NotEmpty
     private User user;
+
+    public String getChapter() {
+        return chapter;
+    }
+
+    public void setChapter(String chapter) {
+        this.chapter = chapter;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
